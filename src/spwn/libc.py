d5e43935dcdcee2b7916adbedaa059604d7a63dc
Matteo Chiesa <matteo.chiesa.mc@gmail.com>
2025-03-05 12:42:03 +0100

    handle requests exception

--- src/spwn/libc.py
+++ src/spwn/libc.py
@@ -71,7 +71,7 @@ class Libc(Binary):
 			waiting.status(f"Downloading from {url}...")
 			try:
 				response = requests.get(url)
-			except requests.ConnectionError as err:
+			except requests.RequestException as err:
 				log.debug(err)
 				response = None
 			if not response:
