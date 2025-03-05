efc30a0b5f5a736b53b0edbe7af55b5d5399537a
Matteo Chiesa <matteo.chiesa.mc@gmail.com>
2025-03-05 09:00:12 +0100

    minor changes in config

--- src/spwn/config.py
+++ src/spwn/config.py
@@ -6,7 +6,7 @@ CONFIG_DIR_PATH = os.path.expanduser("./config_dir")
 CONFIG_FILENAME = "config.json"
 
 DEFAULT_CONFIG = {
-	"debug_dir": "debug_dir",
+	"debug_dir": "debug",
 	"dangerous_functions": ["system", "gets", "ptrace", "memfrob", "strfry", "execve", "execl", "execlp", "execle", "execv", "execvp", "execvpe"],
 	"analyze_seccomp": True,
 	"yara_rules": "~/.config/spwn/findcrypt3.rules",
@@ -16,7 +16,7 @@ DEFAULT_CONFIG = {
 	"do_interactions": False,
 	"template_file": "~/.config/spwn/template.py",
 	"script_basename": "solve_{exe_basename}.py",
-	"pwntube_variable_name": "io",
+	"pwntube_variable": "io",
 	"tab": "\t",
 }
 
@@ -36,7 +36,7 @@ class Config:
 		self.do_interactions: bool			= args.interactions or actual_config["do_interactions"]
 		self.template_file: str | None		= os.path.expanduser(args.template or actual_config["template_file"])
 		self.script_basename: str			= actual_config["script_basename"]
-		self.pwntube_variable_name: str		= actual_config["pwntube_variable_name"]
+		self.pwntube_variable: str			= actual_config["pwntube_variable"]
 		self.tab: str						= actual_config["tab"]
 
 
