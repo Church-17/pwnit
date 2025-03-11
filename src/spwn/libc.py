3e229fef04431350a6e2c18421fc1280591be394
Matteo Chiesa <matteo.chiesa.mc@gmail.com>
2025-03-11 11:35:46 +0100

    handle request exceptions

--- src/spwn/libc.py
+++ src/spwn/libc.py
@@ -11,42 +11,48 @@ class Libc(Binary):
 		super().__init__(name)
 
 		# Retrieve libc id
-		libc_matches = libcdb.query_libc_rip({'buildid': self.buildid.hex()})
-		if libc_matches:
-			self.libc_id = libc_matches[0]['id']
+		with log.progress("Libc version", "Retrieving libc ID from libc.rip...") as waiting:
+			with context.silent:
+				libc_matches = libcdb.query_libc_rip({'buildid': self.buildid.hex()})
+			if libc_matches:
+				self.libc_id = libc_matches[0]['id']
 
-			# Retrieve libc version
-			match = re.search(r"\d+(?:\.\d+)+", self.libc_id)
-			assert match
-			self.libc_version = match.group()
+				# Retrieve libc version
+				match = re.search(r"\d+(?:\.\d+)+", self.libc_id)
+				assert match
+				self.libc_version = match.group()
 
-		else:
-			self.libc_id = None
-			if libc_matches == []:
-				log.warning(f"Recognized libc is not a standard libc")
-
-			# Retrieve libc version
-			with open(self.path, "br") as libc_file:
-				libc_content = libc_file.read()
-			match = re.search(br"release version (\d+(?:\.\d+)+)", libc_content)
-			if match:
-				self.libc_version = match.group(1).decode()
 			else:
-				self.libc_version = None
-				log.warning("Libc version not found in the file")
+				self.libc_id = None
+				waiting.status("Failed to retrieve libc ID from libc.rip, retrieving version from file...")
+				if libc_matches == []:
+					log.warning(f"Recognized libc is not a standard libc")
 
-		# Print libc version and id
-		if self.libc_version:
-			log.info(f"Libc version: {self.libc_version}" + (f" ({self.libc_id})" if self.libc_id else ""))
+				# Retrieve libc version
+				with open(self.path, "br") as libc_file:
+					libc_content = libc_file.read()
+				match = re.search(br"release version (\d+(?:\.\d+)+)", libc_content)
+				if match:
+					self.libc_version = match.group(1).decode()
+				else:
+					self.libc_version = None
+					waiting.failure("Failed to retrieve libc version")
+
+			# Print libc version and id
+			if self.libc_version:
+				waiting.success(f"{self.libc_version}" + (f" ({self.libc_id})" if self.libc_id else ""))
 
 		# Download libs
 		with log.progress("Retrieve libs", "Downloading...") as waiting:
 			with context.silent:
-				self.libs_path = libcdb.download_libraries(self.path)
+				try:
+					self.libs_path = libcdb.download_libraries(self.path)
+				except requests.RequestException:
+					self.libs_path = None
 			if self.libs_path:
 				waiting.success(f"Done ({self.libs_path})")
 			else:
-				waiting.failure()
+				waiting.failure("Failed to download libs")
 
 
 	def download_source(self, dirpath: str = ".") -> None:
@@ -63,7 +69,10 @@ class Libc(Binary):
 			# Get libc source archive
 			url = f"http://ftpmirror.gnu.org/gnu/libc/glibc-{self.libc_version}.tar.gz"
 			waiting.status(f"Downloading from {url}...")
-			response = requests.get(url)
+			try:
+				response = requests.get(url)
+			except requests.RequestException:
+				response = None
 			if not response:
 				waiting.failure(f"Download from {url} failed")
 				return None
