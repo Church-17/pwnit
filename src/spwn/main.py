1ace40ce9afa085a691da1afffe973d77983340b
Matteo Chiesa <matteo.chiesa.mc@gmail.com>
2025-03-14 08:14:28 +0100

    do interactions only if there is exe

--- src/spwn/main.py
+++ src/spwn/main.py
@@ -64,7 +64,7 @@ def main():
 
 	if config.template_path:
 		# Interactions
-		interactions = Interactions(config.pwntube_variable, config.tab) if config.interactions else None
+		interactions = Interactions(config.pwntube_variable, config.tab) if config.interactions and exe else None
 		
 		# Create script
 		template = Template(config.template_path)
