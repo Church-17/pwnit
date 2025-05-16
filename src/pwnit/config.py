da2225857c2eef2002175281051de75fa0034cf6
Matteo Chiesa <matteo.chiesa.mc@gmail.com>
2025-05-16 18:13:55 +0200

    new yaml config

--- src/pwnit/config.py
+++ src/pwnit/config.py
@@ -1,88 +1,62 @@
 from pathlib import Path
-import json
 from pwnit.file_manage import handle_path, check_file, check_dir, download_file
 from pwnit.args import Args
 
-CONFIG_DIR_PATH: Path = handle_path("~/.config/pwnit/")
-CONFIG_FILEPATH = CONFIG_DIR_PATH / "config.json"
-DEFAULT_CONFIG = {
-	"check_functions": ["system", "gets", "ptrace", "memfrob", "strfry", "execve", "execl", "execlp", "execle", "execv", "execvp", "execvpe"],
-	"patch_path": "./debug/<exe_basename>_patched",
-	"seccomp": True,
-	"yara_rules": str(CONFIG_DIR_PATH / "findcrypt3.rules"),
-	"cwe": False,
-	"download_libc_source": False,
-	"template_path": str(CONFIG_DIR_PATH / "template.py"),
-	"interactions": False,
-	"pwntube_variable": "io",
-	"tab": "\t",
-	"script_path": "solve_<exe_basename:>.py",
-	"commands": [],
-}
+CONFIG_DIRPATH: Path = handle_path("~/.config/pwnit/")
+CONFIG_FILEPATH = CONFIG_DIRPATH / "config.yml"
 
 class Config:
 	def __init__(self, args: Args) -> None:
 
 		# Read (and create if necessary) the config
-		actual_config = self.read_config_file()
+		config = self.read_config_file()
 
 		# Set config variables
-		self.check_functions: list[str] = actual_config["check_functions"]
-		patch_path: str | None			= args.patch or actual_config["patch_path"]
-		self.seccomp: bool				= args.seccomp or actual_config["seccomp"]
-		yara_rules: str | None			= args.yara or actual_config["yara_rules"]
-		self.cwe: bool					= args.cwe or actual_config["cwe"]
-		self.download_libc_source: bool	= args.libc_source or actual_config["download_libc_source"]
-		template_path: str | None		= args.template or actual_config["template_path"]
-		self.interactions: bool			= args.interactions or actual_config["interactions"]
-		self.pwntube_variable: str		= actual_config["pwntube_variable"]
-		self.tab: str					= actual_config["tab"]
-		script_path: str | None			= actual_config["script_path"]
-		self.commands: list[str]		= actual_config["commands"]
+		self.check_functions: list[str] = config["check_functions"].get("list", []) if config["check_functions"]["enable"] else []
+		self.patch_path: Path | None	= handle_path(config["patch"]["path"]) if args.patch or config["patch"]["enable"] else None
+		self.seccomp: bool				= True if args.seccomp or config["seccomp"]["enable"] else False
+		self.yara_rules: Path | None	= handle_path(config["yara"]["path"]) if args.yara or config["yara"]["enable"] else None
+		self.libc_source: bool			= True if args.libc_source or config["libc_source"]["enable"] else False
+		self.commands: list[str]		= config.get("commands", [])
 
-		# Handle paths
-		self.patch_path: Path | None = handle_path(patch_path)
-		self.yara_rules: Path | None = handle_path(yara_rules)
-		self.template_path: Path | None = handle_path(template_path)
-		self.script_path: Path | None = handle_path(script_path) or (self.template_path.name if self.template_path else None)
+		template: dict[str] | None		= config["templates"].get(args.template or "default", None)
+		if template:
+			self.template_path: Path | None	= handle_path(template["path"])
+			self.interactions: bool			= args.interactions or template["interactions"]
+			self.pwntube_variable: str		= template["pwntube_variable"]
+			self.tab: str					= template["tab"]
+			self.script_path: str | None	= handle_path(template["script_path"])
 
 		# Handle only mode
 		if args.only:
 			if not args.patch: self.patch_path = None
 			if not args.seccomp: self.seccomp = False
 			if not args.yara: self.yara_rules = None
-			if not args.cwe: self.cwe = False
-			if not args.libc_source: self.download_libc_source = False
+			if not args.libc_source: self.libc_source = False
 			if not args.interactions and not args.template: self.template_path = None
 			if not args.interactions: self.interactions = False
 			self.commands = []
 
 
 	def read_config_file(self) -> dict[str]:
+		import yaml
 
 		# Check if config file exists
 		if not check_file(CONFIG_FILEPATH):
 
 			# If config dir doesn't exists, create it
-			if not check_dir(CONFIG_DIR_PATH):
-				CONFIG_DIR_PATH.mkdir()
-
-			# Write default config file
-			CONFIG_FILEPATH.write_text(json.dumps(DEFAULT_CONFIG, indent='\t'))
+			if not check_dir(CONFIG_DIRPATH):
+				CONFIG_DIRPATH.mkdir()
 
 			# Try to download missing config files
-			download_file(handle_path(DEFAULT_CONFIG["yara_rules"]), "https://raw.githubusercontent.com/polymorf/findcrypt-yara/master/findcrypt3.rules")
-			download_file(handle_path(DEFAULT_CONFIG["template_path"]), "https://raw.githubusercontent.com/Church-17/pwnit/master/resources/template.py")
+			download_file(handle_path(CONFIG_FILEPATH), "https://raw.githubusercontent.com/Church-17/pwnit/master/resources/config.yml")
+			download_file(handle_path(CONFIG_DIRPATH / "findcrypt3.rules"), "https://raw.githubusercontent.com/polymorf/findcrypt-yara/master/findcrypt3.rules")
+			download_file(handle_path(CONFIG_DIRPATH / "template.py"), "https://raw.githubusercontent.com/Church-17/pwnit/master/resources/template.py")
 
-			actual_config = DEFAULT_CONFIG
-		
-		else:
-			# If config file exists, read it
-			actual_config = json.loads(CONFIG_FILEPATH.read_text())
+		# Parse config file
+		with open(CONFIG_FILEPATH, "r") as config_file:
+			config = yaml.safe_load(config_file)
 
-			# Check integrity and restore config file if necessary
-			if set(actual_config) != set(DEFAULT_CONFIG):
-				actual_config = DEFAULT_CONFIG | actual_config
-				CONFIG_FILEPATH.write_text(json.dumps(actual_config, indent='\t'))
+		# TODO: check integrity
 
-		return actual_config
+		return config
